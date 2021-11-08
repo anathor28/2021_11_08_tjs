60cb228773a32eff80db943848ec12e6f35843e6
alex <desorbaix@free.fr>
2021-11-08 14:58:14 +0100

    meo de state sur fonction

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import style from "./Button.module.css";
 const Button = (props) => {
+  const [isClicked, setisClicked] = useState(false);
   console.log(props);
   return (
     <button
-      onClick={(evt) => props.onButtonClick()}
-      className={style.Button}
+      onClick={(evt) => {
+          setisClicked(true);
+          setTimeout(()=>{
+            setisClicked(false);
+          },300);
+          props.onButtonClick()
+          }}
+      className={`${style.Button}${isClicked?' '+style.clicked:''}`}
       style={{
         backgroundColor: props.bgColor,
         color: props.color,
