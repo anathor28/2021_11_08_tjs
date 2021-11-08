a16bc01121cdc38bc72c620b41fd6be07441fb09
alex <desorbaix@free.fr>
2021-11-08 15:02:51 +0100

    object en useState

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import style from "./Button.module.css";
 const Button = (props) => {
-  const [isClicked, setisClicked] = useState(false);
+  const [isClicked, setisClicked] = useState({clicked:false,uneValue:0});
   console.log(props);
   return (
     <button
       onClick={(evt) => {
-          setisClicked(true);
+          setisClicked({...isClicked, clicked:true});
           setTimeout(()=>{
-            setisClicked(false);
-          },300);
+            setisClicked({...isClicked, clicked:false});
+          },30000);
           props.onButtonClick()
           }}
-      className={`${style.Button}${isClicked?' '+style.clicked:''}`}
+      className={`${style.Button}${isClicked.clicked?' '+style.clicked:''}`}
       style={{
         backgroundColor: props.bgColor,
         color: props.color,
