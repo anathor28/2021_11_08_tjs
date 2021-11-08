39ec910dd24c38fe6a3db2f5b6b398cb45862bab
alex <desorbaix@free.fr>
2021-11-08 11:57:15 +0100

    demo mise en oeuvre css module

--- src/App/components/Button2/Button.js
+++ src/App/components/Button2/Button.js
@@ -1,8 +1,8 @@
 import React from "react";
-import  './Button.css'
+import style from './Button.module.css'
 const Button = (props) => {
     console.log(props);
-    return (<button className="Button">{props.text}</button>);
+    return (<button className={style.Button}>{props.text}</button>);
 };
 
 export default Button;
