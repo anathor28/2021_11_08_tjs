68f6ddcf812d6c6a57f68038b81991df955267eb
alex <desorbaix@free.fr>
2021-11-08 11:54:30 +0100

    demo probleme css non module

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button/Button';
+import Button2 from './components/Button2/Button';
 
 function App() {
   return (
     <div className="App">
       <Button text="Button text"/>
+      <Button2 text="Button text"/>
     </div>
   );
 }
