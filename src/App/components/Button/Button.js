0c41f3bf2f33be5f7ab64f40b1287a37197e02f8
alex <desorbaix@free.fr>
2021-11-08 13:38:12 +0100

    mise en place devent click

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -2,7 +2,10 @@ import React from "react";
 import style from './Button.module.css'
 const Button = (props) => {
     console.log(props);
-    return (<button className={style.Button}>{props.children}</button>);
+    return (<button onClick={(evt)=>{
+        console.log(evt);
+        alert('On a clické sur benjamin!!!');
+    }} className={style.Button}>{props.children}</button>);
 };
 
 export default Button;
