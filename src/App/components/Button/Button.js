38096646f95c5a19b89f6ed93c09ffb3c21b90c5
alex <desorbaix@free.fr>
2021-11-08 14:24:06 +0100

    format text

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,23 +1,31 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import style from './Button.module.css'
+import PropTypes from "prop-types";
+import style from "./Button.module.css";
 const Button = (props) => {
-    console.log(props);
-    return (
-    <button onClick={(evt)=>props.onButtonClick()} 
-                    className={style.Button}
-                    style={{backgroundColor:props.bgColor,color:props.color, ...props.style}}
-                    >{props.children}</button>);
+  console.log(props);
+  return (
+    <button
+      onClick={(evt) => props.onButtonClick()}
+      className={style.Button}
+      style={{
+        backgroundColor: props.bgColor,
+        color: props.color,
+        ...props.style,
+      }}
+    >
+      {props.children}
+    </button>
+  );
 };
-Button.propTypes={
-    children: PropTypes.any.isRequired,
-    onButtonClick: PropTypes.func.isRequired,
-    bgColor: PropTypes.string,
-    color: PropTypes.string,
-    style: PropTypes.object,
-}
-Button.defaultProps={
-    children:'Text du button manquant',
-    // bgColor: 'skyblue'
-}
+Button.propTypes = {
+  children: PropTypes.any.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
+  style: PropTypes.object,
+};
+Button.defaultProps = {
+  children: "Text du button manquant",
+  // bgColor: 'skyblue'
+};
 export default Button;
