6d1ae2f96be13875ed70f1e018cb5a3d9b07df68
alex <desorbaix@free.fr>
2021-11-09 17:18:04 +0100

    connexion au store par props + del des props de memeForm pour autonomie de connexion du cmp

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,61 +5,38 @@ import FlexLayout from "./components/FlexLayout/FlexLayout";
 import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 import { ADR_REST, RESSOURCES } from "./config/config";
-const initialCurrentMeme={
-        titre: "",
-        text: "",
-        x: 50,
-        y: 50,
-        color: '#ACACAC',
-        underline: true,
-        italic: true,
-        fontWeight: "900",
-        fontSize: 24,
-        fx: 100,
-        fy: 100,
-        imageId: 0,
-      };
-class App extends React.Component {
-  state: any;
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentMeme: initialCurrentMeme,
-      images: [],
-      memes:[]
-    };
-  }
-  componentDidMount(){
-    const f1=fetch(`${ADR_REST}${RESSOURCES.memes}`).then(f=>f.json());
-    const f2=fetch(`${ADR_REST}${RESSOURCES.images}`).then(f=>f.json());
-    Promise.all([f1,f2]).then(arrResp=>{
-      this.setState({
-        images:arrResp[1],
-        memes:arrResp[0]
-      });
-    });
-  }
-  componentDidUpdate() {}
-  render() {
-    return (
-      <>
+import { ACTIONS_RESSOURCES, initialCurrentMeme } from "./store/store";
+import { connect } from 'react-redux'
+
+const App=(props)=>{
+  return (
+    <>
       <div className="App">
-      <FlexLayout>
-        <MemeViewer
-          meme={this.state.currentMeme}
-          img={this.state.images.find(
-            (e) => e.id === this.state.currentMeme.imageId
-          )}
-        />
-        <MemeForm currentMeme={this.state.currentMeme} images={this.state.images} onCurrentChange={(updatedMeme)=>{
-          this.setState({currentMeme:updatedMeme});
-        }}/>
+        <FlexLayout>
+          <MemeViewer
+            meme={props.current}
+            img={props.images.find(
+              (e) => e.id === props.current.imageId
+            )}
+          />
+          
+          <MemeForm  />
         </FlexLayout>
       </div>
-      <div>{JSON.stringify(this.state)}</div>
-      </>
-    );
-  }
+      <div>{JSON.stringify(props)}</div>
+    </>
+  );
 }
 
-export default App;
+function mapStateToProps(state, ownProps) {
+  return {
+    ...ownProps,
+    images: state.ressources.images,
+    memes: state.ressources.memes,
+    current: state.current
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
