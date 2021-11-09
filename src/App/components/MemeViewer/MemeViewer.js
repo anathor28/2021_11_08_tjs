9def92b3f61c872af248d5ed2d65f5e50c8b0f30
alex <desorbaix@free.fr>
2021-11-09 09:31:56 +0100

    Meo du viewer de meme

--- src/App/components/MemeViewer/MemeViewer.js
+++ src/App/components/MemeViewer/MemeViewer.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './MemeViewer.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./MemeViewer.module.scss";
 
-const MemeViewer = () => (
-  <div className={styles.MemeViewer} data-testid="MemeViewer">
-    MemeViewer Component
-  </div>
-);
+const MemeViewer = (props) => {
+  console.log(props);
+  return (
+    <svg className={styles.MemeViewer} data-testid="MemeViewer" viewBox={`0 0 ${props.img.w} ${props.img.h}`}>
+      <image href={props.img.url} />
+      <text
+        x={props.meme.x}
+        y={props.meme.y}
+        fill={props.meme.color}
+        textDecoration={props.meme.underline ? "underline" : "none"}
+        fontStyle={props.meme.italic?'italic':'normal'}
+        fontSize={props.meme.fontSize}
+        fontWeight={props.meme.fontWeight}
+      >
+        {props.meme.text}
+      </text>
+    </svg>
+  );
+};
 
-MemeViewer.propTypes = {};
+MemeViewer.propTypes = {
+  meme: PropTypes.object.isRequired,
+  img: PropTypes.object,
+};
 
 MemeViewer.defaultProps = {};
 
