84f3ec229d24d01ba71d8e05cedf597e529fcea6
alex <desorbaix@free.fr>
2021-11-10 16:37:16 +0100

    navbar react-bootstrap@5 + LinkContainer

--- src/App/components/Navbar/Navbar.js
+++ src/App/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Navbar.module.scss';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.scss";
+import { Link } from "react-router-dom";
+import { Nav, Navbar as NavBar, Container } from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.css';
 const Navbar = () => (
-  <div className={styles.Navbar} data-testid="Navbar">
-        <ul>
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/thumbnail">Thumbnail</Link></li>
-          <li><Link to="/editor">Editor</Link></li>
-          <li><Link to="/editor/0">Editor/0</Link></li>
-        </ul>
-  </div>
+  <NavBar bg="primary" variant="dark">
+    <Container>
+      <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+      <Nav className="me-auto">
+        <LinkContainer to="/">
+          <Nav.Link>Home</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/editor">
+          <Nav.Link>New</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/thumbnail">
+          <Nav.Link>Thumbnail</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </Container>
+  </NavBar>
 );
 
 Navbar.propTypes = {};
