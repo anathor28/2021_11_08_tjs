29aed8dd3763b873c7bde66fc12577a6ece61214
alex <desorbaix@free.fr>
2021-11-10 11:30:52 +0100

    MEO de routes en v6 de react-router-dom

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,28 +8,42 @@ import { ADR_REST, RESSOURCES } from "./config/config";
 import { ACTIONS_RESSOURCES, initialCurrentMeme } from "./store/store";
 import { connect } from "react-redux";
 import ThumbnailLayout from "./components/ThumbnailLayout/ThumbnailLayout";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = (props) => {
   return (
     <>
+      <Navbar />
       <div className="App">
-        <ThumbnailLayout>
-          {props.memes.map((e, i) => (
-            <MemeViewer
-              meme={e}
-              img={props.images.find((ee) => ee.id === e.imageId)}
-              key={"thumbnail-" + i}
-            />
-          ))}
-        </ThumbnailLayout>
-        <FlexLayout>
-          <MemeViewer
-            meme={props.current}
-            img={props.images.find((e) => e.id === props.current.imageId)}
-          />
+        <Routes>
+          <Route path="/*" element={
+            <>
+              <h1>Deguemer mat in breizh meme generator</h1>
+              <h2>Selectionner dans le menu</h2>
+              le liens a afficher dans la page
+            </>}/>
+          <Route path="/thumbnail" element={
+            <ThumbnailLayout>
+              {props.memes.map((e, i) => (
+                <MemeViewer
+                  meme={e}
+                  img={props.images.find((ee) => ee.id === e.imageId)}
+                  key={"thumbnail-" + i}
+                />
+              ))}
+            </ThumbnailLayout>}/>
+          
+          <Route path="/editor" element={
+            <FlexLayout>
+              <MemeViewer
+                meme={props.current}
+                img={props.images.find((e) => e.id === props.current.imageId)}
+              />
 
-          <MemeForm />
-        </FlexLayout>
+              <MemeForm />
+            </FlexLayout>}/>
+        </Routes>
       </div>
       <div>{JSON.stringify(props)}</div>
     </>
