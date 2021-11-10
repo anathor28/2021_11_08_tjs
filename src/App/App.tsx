722fcee77178e3c38870e46f174fd405a643b0a2
alex <desorbaix@free.fr>
2021-11-10 10:56:09 +0100

    mise en place du thumbnail

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,37 +6,45 @@ import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 import { ADR_REST, RESSOURCES } from "./config/config";
 import { ACTIONS_RESSOURCES, initialCurrentMeme } from "./store/store";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import ThumbnailLayout from "./components/ThumbnailLayout/ThumbnailLayout";
 
-const App=(props)=>{
+const App = (props) => {
   return (
     <>
       <div className="App">
+        <ThumbnailLayout>
+          {props.memes.map((e, i) => (
+            <MemeViewer
+              meme={e}
+              img={props.images.find((ee) => ee.id === e.imageId)}
+              key={"thumbnail-" + i}
+            />
+          ))}
+        </ThumbnailLayout>
         <FlexLayout>
           <MemeViewer
             meme={props.current}
-            img={props.images.find(
-              (e) => e.id === props.current.imageId
-            )}
+            img={props.images.find((e) => e.id === props.current.imageId)}
           />
-          
-          <MemeForm  />
+
+          <MemeForm />
         </FlexLayout>
       </div>
       <div>{JSON.stringify(props)}</div>
     </>
   );
-}
+};
 
 function mapStateToProps(state, ownProps) {
   return {
     ...ownProps,
     images: state.ressources.images,
     memes: state.ressources.memes,
-    current: state.current
-  }
+    current: state.current,
+  };
 }
 function mapDispatchToProps(dispatch) {
-  return {}
+  return {};
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
